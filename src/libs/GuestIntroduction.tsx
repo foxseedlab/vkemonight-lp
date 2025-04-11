bdbf635408b4dfba80f7893019425a06f4a2301e
Foxseed <takara2314_TK@outlook.jp>
2025-04-12 00:58:49 +0900

    feat: レスポンシブ化

--- src/libs/GuestIntroduction.tsx
+++ src/libs/GuestIntroduction.tsx
@@ -12,10 +12,12 @@ type Props = {
 export function GuestIntroduction({ imageSrc, name, positions, description }: Props) {
   return (
     <div className="mt-[6rem] w-full bg-primary/30 border-2 border-primary/20 relative">
-      <figure className="md:w-[20rem] xl:w-[28rem] h-[calc(100%+6rem)] bg-white/50 absolute bottom-0">
+      <figure className="w-full md:w-[20rem] xl:w-[28rem] h-[36rem] md:h-[calc(100%+6rem)]
+        bg-white/50 absolute -top-[6rem] bottom-auto md:top-auto md:bottom-0">
       </figure>
 
-      <div className="ml-[20rem] xl:ml-[28rem] w-[calc(100%-20rem)] xl:w-[calc(100%-28rem)] p-10">
+      <div className="mt-[32rem] md:mt-0 md:ml-[20rem] xl:ml-[28rem]
+        w-full md:w-[calc(100%-20rem)] xl:w-[calc(100%-28rem)] p-6 md:p-10">
         <Header2 title={name} className="mb-2" />
         <Position positions={positions}/>
         <Paragraph className="mt-4">
