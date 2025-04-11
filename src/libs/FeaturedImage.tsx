976c1382421b6343103d7868721733b6f640a2d6
Foxseed <takara2314_TK@outlook.jp>
2025-04-11 23:57:56 +0900

    feat: その他の要素もレスポンシブ化

--- src/libs/FeaturedImage.tsx
+++ src/libs/FeaturedImage.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export default function FeaturedImage({ imageSrc }: Props) {
   return (
-    <figure className="w-1/2">
+    <figure className="w-full md:w-1/2">
       <div className="w-full aspect-square bg-gray-900" />
     </figure>
   )
