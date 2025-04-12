f6220048b3692f876596ec258c7f951172498c90
Foxseed <takara2314_TK@outlook.jp>
2025-04-12 17:26:13 +0900

    feat: 複数ゲストに対応

--- src/libs/GuestIntroduction.tsx
+++ src/libs/GuestIntroduction.tsx
@@ -1,29 +1,37 @@
-import Header2 from "./headers/Header2";
-import Paragraph from "./headers/Paragraph";
-import Position, { type PositionType } from "./Position";
+import Position, { type PositionType } from './Position';
+import Header2 from './headers/Header2';
+import Paragraph from './headers/Paragraph';
 
 type Props = {
   imageSrc: string;
   name: string;
   positions: PositionType[];
   description: string;
-}
+};
 
-export function GuestIntroduction({ imageSrc, name, positions, description }: Props) {
+export default function GuestIntroduction({
+  imageSrc,
+  name,
+  positions,
+  description,
+}: Props) {
   return (
-    <div className="mt-[6rem] w-full bg-primary/30 border-2 border-primary/20 relative">
-      <figure className="w-full md:w-[20rem] xl:w-[28rem] h-[36rem] md:h-[calc(100%+6rem)]
-        bg-white/50 absolute -top-[6rem] bottom-auto md:top-auto md:bottom-0">
-      </figure>
+    <li className="mt-[6rem] w-full bg-primary/30 border-2 border-primary/20 relative">
+      <figure
+        className="w-full md:w-[20rem] xl:w-[28rem] h-[36rem] md:h-[calc(100%+6rem)]
+        bg-white/50 absolute -top-[6rem] bottom-auto md:top-auto md:bottom-0"
+      />
 
-      <div className="mt-[32rem] md:mt-0 md:ml-[20rem] xl:ml-[28rem]
-        w-full md:w-[calc(100%-20rem)] xl:w-[calc(100%-28rem)] p-6 md:p-10">
+      <div
+        className="mt-[32rem] md:mt-0 md:ml-[20rem] xl:ml-[28rem]
+        w-full md:w-[calc(100%-20rem)] xl:w-[calc(100%-28rem)] p-6 md:p-10"
+      >
         <Header2 title={name} className="mb-2" />
-        <Position positions={positions}/>
+        <Position positions={positions} />
         <Paragraph className="mt-4">
           {description}
         </Paragraph>
       </div>
-    </div>
-  )
+    </li>
+  );
 }
