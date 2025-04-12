61003d5fb3c8736320ecef9e21946d32ea46d714
Foxseed <takara2314_TK@outlook.jp>
2025-04-13 00:12:35 +0900

    refactor: import 時に type 指定は外に出すように

--- src/libs/headers/Paragraph.tsx
+++ src/libs/headers/Paragraph.tsx
@@ -1,14 +1,10 @@
-import { type ReactNode } from 'react';
+import type { ReactNode } from 'react';
 
 type Props = {
   children: ReactNode;
   className?: string;
-}
+};
 
 export default function Paragraph({ children, className = '' }: Props) {
-  return (
-    <p className={`tracking-wide leading-10 ${className}`}>
-      {children}
-    </p>
-  );
+  return <p className={`tracking-wide leading-10 ${className}`}>{children}</p>;
 }
