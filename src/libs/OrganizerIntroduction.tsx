7388916bafa51e3b523a1686796d361fb0a13ebd
Foxseed <takara2314_TK@outlook.jp>
2025-04-13 00:40:38 +0900

    feat: スタッフにも画像が入るように

--- src/libs/OrganizerIntroduction.tsx
+++ src/libs/OrganizerIntroduction.tsx
@@ -3,22 +3,25 @@ import Paragraph from './headers/Paragraph';
 import type { PositionType } from './stores/people';
 
 type Props = {
-  imageSrc: string;
   name: string;
   positions: PositionType[];
   description: string;
+  avatarUrl: string;
 };
 
 export default function OrganizerIntroduction({
-  imageSrc,
   name,
   positions,
   description,
+  avatarUrl,
 }: Props) {
   return (
     <li className="mt-6 w-[calc(100%-1.5rem)] bg-primary/30 border-2 border-primary/20 relative">
       <div className="w-full flex flex-row relative -top-6 -left-6">
-        <figure className="w-32 h-32 bg-gray-100 border-1 border-secondary/30" />
+        <figure className="w-32 h-32 border-2 border-primary-20">
+          <img src={avatarUrl} alt={name} className="w-full" />
+        </figure>
+
         <div className="pt-10 px-6">
           <h2 className="mb-1 text-lg font-medium">{name}</h2>
           <Position positions={positions} />
