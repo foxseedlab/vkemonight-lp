a010d415da65f1b865b913a9c1210a7f66c60eed
Foxseed <takara2314_TK@outlook.jp>
2025-04-16 21:32:26 +0900

    feat: アイキャッチ画像がスライドするように

--- src/libs/FeaturedImage.tsx
+++ src/libs/FeaturedImage.tsx
@@ -1,17 +1,54 @@
+import { Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
+import type { Image } from './stores/init';
+
 type Props = {
-  imageSrc: string;
+  image: Image;
 };
 
-export default function FeaturedImage({ imageSrc }: Props) {
+export default function FeaturedImage({ image }: Props) {
   return (
     <figure className="w-full md:w-1/2">
       <div className="w-full aspect-square">
         <img
-          src={imageSrc}
+          src={image.url}
           alt="アイキャッチ画像"
           className="w-full h-full object-cover"
         />
       </div>
+    </figure>
+  );
+}
+
+export function FeaturedImageSlide({ images }: { images: Image[] }) {
+  const intervalMilliseconds = 3000;
+
+  return (
+    <figure className="w-full md:w-1/2">
+      <Swiper
+        modules={[Autoplay]}
+        spaceBetween={0}
+        slidesPerView={1}
+        autoplay={{
+          delay: intervalMilliseconds,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+      >
+        {images.map((image, index) => (
+          <SwiperSlide key={image.url}>
+            <div className="w-full aspect-square">
+              <img
+                src={image.url}
+                alt={`アイキャッチ画像${index + 1}`}
+                className="w-full h-full object-cover"
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </figure>
   );
 }
