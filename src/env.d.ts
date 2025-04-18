1cfc5904191d18d75699bb7cdfaa9c35cf1a6066
Foxseed <takara2314_TK@outlook.jp>
2025-04-19 04:40:46 +0900

    feat: ボタンの推移先を確定

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,9 @@
 type ImportMetaEnv = {
   readonly MICROCMS_SERVICE_DOMAIN: string;
   readonly MICROCMS_API_KEY: string;
+
+  readonly PUBLIC_X_URL: string;
+  readonly PUBLIC_KOFI_URL: string;
 };
 
 interface ImportMeta {
