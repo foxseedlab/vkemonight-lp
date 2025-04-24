ec748e4d317aa55a29863261bc0d787f1dd35ea2
Foxseed <takara2314_TK@outlook.jp>
2025-04-25 05:25:19 +0900

    feat: 画像読み込みを強化

--- src/libs/imgix/image.ts
+++ src/libs/imgix/image.ts
@@ -1,27 +1,40 @@
 export function recommendFeaturedSrc(src: string) {
   const format = 'webp';
   const width = 1080;
+  const quality = 85;
+  const compression = 'webp-lossless-compression';
 
-  return `${src}?w=${width}&fm=${format}`;
+  return `${src}?w=${width}&fm=${format}&q=${quality}&auto=compress,format&${compression}=1`;
 }
 
 export function recommendStillSrc(src: string) {
   const format = 'webp';
   const width = 512;
+  const quality = 85;
 
-  return `${src}?w=${width}&fm=${format}`;
+  return `${src}?w=${width}&fm=${format}&q=${quality}&auto=compress,format`;
 }
 
 export function recommendAvatarSrc(src: string) {
   const format = 'webp';
   const width = 200;
+  const quality = 85;
 
-  return `${src}?w=${width}&fm=${format}`;
+  return `${src}?w=${width}&fm=${format}&q=${quality}&auto=compress,format`;
 }
 
 export function recommendLogoSrc(src: string) {
   const format = 'webp';
   const width = 512;
+  const quality = 90;
 
-  return `${src}?w=${width}&fm=${format}`;
+  return `${src}?w=${width}&fm=${format}&q=${quality}&auto=compress,format`;
+}
+
+export function recommendPlaceholderSrc(src: string) {
+  const format = 'webp';
+  const width = 20;
+  const quality = 50;
+
+  return `${src}?w=${width}&fm=${format}&q=${quality}&blur=200`;
 }
